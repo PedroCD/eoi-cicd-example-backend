05eea3add9563bb011e9d3d1d1a686666b4f1464
PedroCD <camejodiazpedro@gmail.com>
2022-07-19 18:20:17 +0100

    some more troll comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI} Y ME PICA EL CULO`);
+    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI} Y NO TENGO UÑAS PA RASCARME`);
 });
 
 app.listen(process.env.PORT || 3000, () => {
