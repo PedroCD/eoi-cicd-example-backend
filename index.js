41c4ac32f8edc9d2218e7659aae359bf16a44dd5
PedroCD <camejodiazpedro@gmail.com>
2022-07-19 18:23:27 +0100

    trying

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI} Y ME PICA EL CULO`);
+    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI} Y ME PICA EL CULO
+    HOLAAAAAAAAAAAAAAAAAAAAAAAAA`);
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Example app listening on port 3000!');
